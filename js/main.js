ca9f00a1dd3783cc8d5fdf72038468d2fbc3cb9d
Gloria Fernández <gloriafercu@gmail.com>
2018-05-08 17:40:17 +0200

    change order id

--- js/main.js
+++ js/main.js
@@ -2,44 +2,44 @@ function application() {
 	function getPairQuestionAnswers(callback) {
 		var serverData = [
 			{
-				id: 1,
+				id: 0,
 				question: '¿Cuáles son los nombres de Estela?',
 				answers: [
-					{ id: 1, value: 'Estela' },
-					{ id: 2, value: 'Rita' },
-					{ id: 3, value: 'Todas las anteriores son correctas' },
+					{ id: 0, value: 'Estela' },
+					{ id: 1, value: 'Rita' },
+					{ id: 2, value: 'Todas las anteriores son correctas' },
 				],
-				correctAnswer: { id: 3 }
+				correctAnswer: { id: 2 }
+			},
+			{
+				id: 1,
+				question: '¿Cuál es la capital de Zambia?',
+				answers: [
+					{ id: 0, value: 'Lusaka' },
+					{ id: 1, value: 'Harare' },
+					{ id: 2, value: 'Madrid' },
+				],
+				correctAnswer: { id: 1 }
 			},
 			{
 				id: 2,
-				question: '¿Cuál es la capital de Zambia?',
+				question: '¿Cuál es el nombre completo de Freud?',
 				answers: [
-					{ id: 1, value: 'Lusaka' },
-					{ id: 2, value: 'Harare' },
-					{ id: 3, value: 'Madrid' },
+					{ id: 0, value: 'Adolf' },
+					{ id: 1, value: 'Sefard' },
+					{ id: 2, value: 'Sigmund' },
 				],
 				correctAnswer: { id: 2 }
 			},
 			{
 				id: 3,
-				question: '¿Cuál es el nombre completo de Freud?',
-				answers: [
-					{ id: 1, value: 'Adolf' },
-					{ id: 2, value: 'Sefard' },
-					{ id: 3, value: 'Sigmund' },
-				],
-				correctAnswer: { id: 3 }
-			},
-			{
-				id: 4,
 				question: '¿Cuál es el animal más rápido del mundo?',
 				answers: [
-					{ id: 1, value: 'Guepardo' },
-					{ id: 2, value: 'León' },
-					{ id: 3, value: 'Tortuga' },
+					{ id: 0, value: 'Guepardo' },
+					{ id: 1, value: 'León' },
+					{ id: 2, value: 'Tortuga' },
 				],
-				correctAnswer: { id: 1 }
+				correctAnswer: { id: 0 }
 			}
 		];
 		callback(serverData);
@@ -71,10 +71,10 @@ function application() {
 
 		if (i < questions.length) {
 			quizQuestion.innerHTML = questions[i].question;
-			quizQuestion.setAttribute('id', (i +1));
+			quizQuestion.setAttribute('id', i);
 			var answersList = "";
 			for (let x = 0; x < questions[i].answers.length; x++) {
-				answersList += '<li id="'+ (x + 1) + '" class="li-answers"><input id="' + (x + 1) + '" type="radio" name="answers"/><label for="' + (x + 1) + '">' + questions[i].answers[x].value + '</label></li>';
+				answersList += '<li id="'+ x + '" class="li-answers"><input id="' + x  + '" type="radio" name="answers"/><label for="' + x + '">' + questions[i].answers[x].value + '</label></li>';
 				quizAnswers.innerHTML = answersList;
 			}
 			i++;
@@ -88,18 +88,26 @@ function application() {
 
 		for (var i = 0; i < currentAnswers.length; i++) {
 			if (currentAnswers[i].checked) {
-				currentAnswerId = i + 1;
+				currentAnswerId = i;
+				if (questions[currentQuestionId].correctAnswer.id == currentAnswerId) {
+					console.log ('Es correcto!');
+				} else {
+					console.log('Es falso!');
+
+				}
 				console.log(currentAnswerId);
 				return currentAnswerId;
 			};
 		}
+
 	}
 
 	function start() {
-		seconds = 5;
+		seconds = 100;
 		var counter = setInterval(questionTimer, 1000);
 		var nextQuestionButton = document.querySelector('.next-question');
-		nextQuestionButton.addEventListener('click', checkOption);
+		var sendAnswerButton = document.querySelector('.send-answer');
+		sendAnswerButton.addEventListener('click', checkOption);
 		nextQuestionButton.addEventListener('click', nextQuestion);
 	}
 
