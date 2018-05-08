d3c9e4078c8b1ae148b1601e8367753ea6323d8f
gloriafercu <gloriafercu@gmail.com>
2018-05-08 13:11:29 +0200

    add function application

--- js/main.js
+++ js/main.js
@@ -1,93 +1,105 @@
-function getPairQuestionAnswers(callback) {
-    var serverData = [
-        {
-            id: 1,
-            question: '¿Cuáles son los nombres de Estela?',
-            answers: [
-                { id: 1, value: 'Estela' },
-                { id: 2, value: 'Rita' },
-                { id: 3, value: 'Todas las anteriores son correctas' },
-            ],
-            correctAnswer: { id: 3 }
-        },
-        {
-            id: 2,
-            question: '¿Cuál es la capital de Zambia?',
-            answers: [
-                { id: 1, value: 'Lusaka' },
-                { id: 2, value: 'Harare' },
-                { id: 3, value: 'Madrid' },
-            ],
-            correctAnswer: { id: 2 }
-        },
-        {
-            id: 3,
-            question: '¿Cuál es el nombre completo de Freud?',
-            answers: [
-                { id: 1, value: 'Adolf' },
-                { id: 2, value: 'Sefard' },
-                { id: 3, value: 'Sigmund' },
-            ],
-            correctAnswer: { id: 3 }
-        },
-        {
-            id: 4,
-            question: '¿Cuál es el animal más rápido del mundo?',
-            answers: [
-                { id: 1, value: 'Guepardo' },
-                { id: 2, value: 'León' },
-                { id: 3, value: 'Tortuga' },
-            ],
-            correctAnswer: { id: 1 }
-        }
-    ];
-    callback(serverData);
+function application() {
+
+
+
+	function getPairQuestionAnswers(callback) {
+		var serverData = [
+			{
+				id: 1,
+				question: '¿Cuáles son los nombres de Estela?',
+				answers: [
+					{ id: 1, value: 'Estela' },
+					{ id: 2, value: 'Rita' },
+					{ id: 3, value: 'Todas las anteriores son correctas' },
+				],
+				correctAnswer: { id: 3 }
+			},
+			{
+				id: 2,
+				question: '¿Cuál es la capital de Zambia?',
+				answers: [
+					{ id: 1, value: 'Lusaka' },
+					{ id: 2, value: 'Harare' },
+					{ id: 3, value: 'Madrid' },
+				],
+				correctAnswer: { id: 2 }
+			},
+			{
+				id: 3,
+				question: '¿Cuál es el nombre completo de Freud?',
+				answers: [
+					{ id: 1, value: 'Adolf' },
+					{ id: 2, value: 'Sefard' },
+					{ id: 3, value: 'Sigmund' },
+				],
+				correctAnswer: { id: 3 }
+			},
+			{
+				id: 4,
+				question: '¿Cuál es el animal más rápido del mundo?',
+				answers: [
+					{ id: 1, value: 'Guepardo' },
+					{ id: 2, value: 'León' },
+					{ id: 3, value: 'Tortuga' },
+				],
+				correctAnswer: { id: 1 }
+			}
+		];
+		callback(serverData);
+	}
+
+	var questions = [];
+	getPairQuestionAnswers(function (data) {
+		questions = data;
+	});
+
+	var quizContainer = document.querySelector('.show-quiz');
+	var quizQuestion = document.querySelector('.quiz-questions');
+	var quizAnswers = document.querySelector('.quiz-answers');
+	var nextQuestionButton = document.querySelector('.next-question');
+
+	function questionTimer() {
+		seconds--;
+		var timer = document.querySelector('.timer');
+		timer.innerHTML = seconds;
+		if (seconds === 0) {
+			clearInterval(counter);
+			nextQuestion();
+		}
+	}
+	var counter = setInterval(questionTimer, 1000);
+
+	var i = 0;
+	function nextQuestion() {
+		seconds = 20;
+		questionTimer();
+		if (i < questions.length) {
+			quizQuestion.innerHTML = questions[i].question;
+			var answersList = "";
+			for (let x = 0; x < questions[i].answers.length; x++) {
+				answersList += '<li class="li-answers"><input id="' + (x + 1) + '" type="radio" name="answers"/><label for="' + (x + 1) + '">' + questions[i].answers[x].value + '</label></li>';
+				quizAnswers.innerHTML = answersList;
+			}
+			i++;
+		}
+
+	}
+
+	nextQuestionButton.addEventListener('click', nextQuestion);
+
+	var seconds = 20;
+	questionTimer();
+
+	var counter = setInterval(questionTimer, 1000);
+
+
+
+	function start() {
+
+	}
+
+
+	return {
+		start: start
+	}
 }
-
-var questions = [];
-getPairQuestionAnswers(function (data) {
-    questions = data;
-});
-
-var quizContainer = document.querySelector('.show-quiz');
-var quizQuestion = document.querySelector('.quiz-questions');
-var quizAnswers = document.querySelector('.quiz-answers');
-var nextQuestionButton = document.querySelector('.next-question');
-
-function questionTimer() {
-    seconds--;
-    var timer = document.querySelector('.timer');
-    timer.innerHTML = seconds;
-    if (seconds === 0) {
-        clearInterval(counter);
-        nextQuestion();
-    }
-}
-var counter = setInterval(questionTimer, 1000);
-
-var i = 0;
-function nextQuestion() {
-    seconds = 20;
-    questionTimer();
-    if (i < questions.length) {
-        quizQuestion.innerHTML = questions[i].question;
-        var answersList = "";
-        for (let x = 0; x < questions[i].answers.length; x++) {
-            answersList += '<li class="li-answers"><input id="' + (x + 1) + '" type="radio" name="answers"/><label for="' + (x + 1) + '">' + questions[i].answers[x].value + '</label></li>';
-            quizAnswers.innerHTML = answersList;
-        }
-        i++;
-    }
-
-}
-
-nextQuestionButton.addEventListener('click', nextQuestion);
-
-var seconds = 20;
-questionTimer();
-
-var counter = setInterval(questionTimer, 1000);
-
-//
-// const option = document.querySelector('input[name="anwers"]:checked').value;
-// console.log('option ', option);
