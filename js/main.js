9a56590e93762d01829e59f989b3990acba506db
Gloria Fernández <gloriafercu@gmail.com>
2018-05-08 17:22:31 +0200

    setAttributesId to questions and answers

--- js/main.js
+++ js/main.js
@@ -46,6 +46,7 @@ function application() {
 	}
 
 	var questions = [];
+
 	getPairQuestionAnswers(function (data) {
 		questions = data;
 	});
@@ -56,25 +57,41 @@ function application() {
 		var timer = document.querySelector('.timer');
 		timer.innerHTML = seconds;
 		if (seconds === 0) {
-			seconds = 5;
+			seconds = 100;
 			nextQuestion();
 		}
 	}
 
 	var i = 0;
+	var quizQuestion = document.querySelector('.quiz-questions');
+
 	function nextQuestion() {
 		var quizContainer = document.querySelector('.show-quiz');
-		var quizQuestion = document.querySelector('.quiz-questions');
 		var quizAnswers = document.querySelector('.quiz-answers');
 
 		if (i < questions.length) {
 			quizQuestion.innerHTML = questions[i].question;
+			quizQuestion.setAttribute('id', (i +1));
 			var answersList = "";
 			for (let x = 0; x < questions[i].answers.length; x++) {
-				answersList += '<li class="li-answers"><input id="' + (x + 1) + '" type="radio" name="answers"/><label for="' + (x + 1) + '">' + questions[i].answers[x].value + '</label></li>';
+				answersList += '<li id="'+ (x + 1) + '" class="li-answers"><input id="' + (x + 1) + '" type="radio" name="answers"/><label for="' + (x + 1) + '">' + questions[i].answers[x].value + '</label></li>';
 				quizAnswers.innerHTML = answersList;
 			}
 			i++;
+		}
+	}
+
+	function checkOption() {
+		var currentAnswers = document.getElementsByTagName('input');
+		var currentAnswerId;
+		var currentQuestionId = quizQuestion.getAttribute('id');
+
+		for (var i = 0; i < currentAnswers.length; i++) {
+			if (currentAnswers[i].checked) {
+				currentAnswerId = i + 1;
+				console.log(currentAnswerId);
+				return currentAnswerId;
+			};
 		}
 	}
 
@@ -82,11 +99,12 @@ function application() {
 		seconds = 5;
 		var counter = setInterval(questionTimer, 1000);
 		var nextQuestionButton = document.querySelector('.next-question');
+		nextQuestionButton.addEventListener('click', checkOption);
 		nextQuestionButton.addEventListener('click', nextQuestion);
 	}
 
-
 	return {
-		start: start
+		start: start,
+		nextQuestion: nextQuestion
 	}
 }
